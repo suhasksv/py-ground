378f9af73c1716172a3b64141b205e875e97e588
Suhas K <suhas.ksv@gmail.com>
2020-05-29 11:22:52 +0530

    Calculator config added

--- CodeCrakers/Calculator.py
+++ CodeCrakers/Calculator.py
@@ -1,4 +1,4 @@
-num = int(input("How many numbers would like to oparate? "))
+"""num = int(input("How many numbers would like to oparate? "))
 s = a = f = ask = 0
 for i in range(0, num-1):
     if i == 0:
@@ -27,3 +27,22 @@ for i in range(0, num-1):
     if ask == "**":
         s = s ** f        
 print(s)
+"""
+def add(a,b):
+    return (a+b)
+def sub(a,b):
+    return (a-b)
+def mul(a,b):
+    return (a*b)
+def div(a,b):
+    return (a/b)
+def power(a,b):
+    return (a**b)
+
+num1 = int(input("Enter the first number:"))
+op1 = input("Enter the first operation:")
+num2 = int(input("Enter the second number:"))
+
+def operation():
+    if "+" or "add" or "addition" or "sum":
+        return add(num1, num2)
