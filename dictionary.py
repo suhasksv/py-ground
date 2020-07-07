7b2edbb46df985c5bd986b2ba1548fb196752013
Suhas K <suhas.ksv@gmail.com>
2020-07-07 16:49:45 +0530

    Dictionaries update

--- dictionary.py
+++ dictionary.py
@@ -16,3 +16,25 @@ print(plang)
 
 for i in plang:
     print(i)
+
+fruits = {"Apples": 20, "Bananas": 40, "Mangoes": 50, "cherries": 100}
+print(fruits)
+
+for i in fruits:
+    print(i)
+
+del fruits["cherries"]
+print(fruits)
+
+for i in fruits:
+    print(i)
+
+fruits["Apples"] = 30
+fruits["Watermelon"] = 2
+print(fruits)
+
+for i in fruits:
+    print(i)
+print("    ")
+if "Watermelon" in fruits:
+    print("It is There")
