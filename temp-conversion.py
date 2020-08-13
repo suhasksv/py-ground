ce3fc7ac607c91dc7bd78becd99090f4d8c202ac
Suhas K <suhas.ksv@gmail.com>
2020-08-13 18:46:05 +0530

    Temperature Conversion

---
+++ temp-conversion.py
@@ -0,0 +1,9 @@
+print("Converting F to C")
+fe = float(input("Enter the Temperature:"))
+cel = (fe - 32.0) * 5.0 / 9.0
+print(cel)
+"""
+print("Converting C to F")
+ce = float(input("Enter the Temperature:"))
+fe =
+"""
