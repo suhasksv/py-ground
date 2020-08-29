f20ea01680925bd94174bdd3e272d51a91f32fad
Suhas K <suhas.ksv@gmail.com>
2020-08-29 12:52:55 +0530

    Calculator.py update

--- CodeCrakers/Calculator.py
+++ CodeCrakers/Calculator.py
@@ -12,38 +12,51 @@ time.sleep(1.0)
 num1 = int(input("Enter an integer:"))
 
 
+# Calculating addition
 def add(a, b):
     return a + b
 
 
+# Calculating subtraction
 def sub(a, b):
     return a - b
 
 
+# Calculating multiplication
 def mul(a, b):
     return a * b
 
 
+# Calculating division
 def div(a, b):
     return a / b
 
 
+# Calculating exponent
 def power(a, b):
     return a ** b
 
 
-ops = {"+": add, "add": add, "addition": add, "sum": add, "-": sub, "sub": sub, "subtraction": sub, "difference": sub, "x": mul,
-       "multiply": mul, "*": mul, "multiplication": mul, "div": div, "division": div, "/": div, "power": power, "^": power, "**": power}
+# Calculating mod
+def mod(a, b):
+    return a % b
 
+
+# Defining a Dictionary of Operations
+ops = {"+": add, "add": add, "addition": add, "sum": add, "-": sub, "sub": sub, "subtraction": sub, "difference": sub,
+       "x": mul,
+       "multiply": mul, "*": mul, "multiplication": mul, "div": div, "division": div, "/": div, "power": power,
+       "^": power,
+       "**": power, "mod": mod, "%": mod, "||": mod, "| |": mod, }
+# Main loop of calculation
 while True:
-    op = input("Enter operation:")
+    op = input("Enter operation:")              # Takes input from the costumer and stores in variable op
 
     if op.lower() in ops:
         num2 = int(input("Enter an integer:"))
 
-        q = eval("ops[op](num1, num2)")
+        q = eval("ops[op](num1, num2)")         # eval is function in python that evaluates the value of the given operation(op)
         print(q)
         num1 = q
     else:
         print("Sorry Operation does not exist. Please try another operation.")
-        break
