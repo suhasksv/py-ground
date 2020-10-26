052c1b8c8f268d5917ed612de0f486096dc21493
Suhas K <suhas.ksv@gmail.com>
2020-10-26 07:33:17 +0530

    Update

--- CodeCrakers/number-pyramid1-factorial.py
+++ CodeCrakers/number-pyramid1-factorial.py
@@ -6,14 +6,40 @@ def factorial(curr):
 		g *= i
 	return g
 
-try:
-	row = int(input("Enter number of rows:"))
-except:
-	print("Invalid input. Please enter an integer")
-	exit(1)
+def non_recurrsion():
+	try:
+		row = int(input("Enter number of rows:"))
+	except:
+		print("Invalid input. Please enter an integer")
+		exit(1)
 
-for i in range(1, row+1):
-	for j in range(0, i):
-		print(factorial(num), end = " ")
-		num += 1
-	print("")
+	for i in range(1, row+1):
+		for j in range(0, i):
+			print(factorial(num), end = " ")
+			num += 1
+		print("")
+
+
+# Recursive statement included
+def factorial_recursion(numb):
+	if numb == 1:
+		return 1
+	else:
+		return numb * factorial_recursion(numb-1)
+
+def recursion():
+	try:
+			row = int(input("Enter number of rows:"))
+	except:
+		print("Invalid input. Please enter an integer")
+		exit(1)
+
+	for i in range(1, row+1):
+		for j in range(0, i):
+			print(factorial_recursion(num), end = " ")
+			num += 1
+		print("")
+
+non_recurrsion()
+
+recursion()
--- CodeCrakers/number-pyramid1.py
+++ CodeCrakers/number-pyramid1.py
@@ -1,19 +1,13 @@
-
-def factorial(numb):
-	if numb == 1:
-		return 1
-	else:
-		return numb * factorial(numb-1)
-
-
-num = 1
-try:
-    row = int(input("Enter number of rows:"))
-except:
-    print("Please enter integer")
-    exit(1)
-for i in range(1, row+1):
-    for j in range(0, i):
-        print(factorial(num), end=" ")
-        num += 1
-    print("")
+def main():
+    num = 1
+    try:
+        row = int(input("Enter number of rows:"))
+    except:
+        print("Please enter integer")
+        exit(1)
+    for i in range(1, row+1):
+        for j in range(0, i):
+            print("{:>2}".format(num), end=" ")
+            num += 1
+        print("")
+main()
