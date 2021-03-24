1d775d2f77aa6d5cc2e2b5149f9a8ec7b84479bc
Suhas K <suhas.ksv@gmail.com>
2021-03-24 16:59:32 +0530

    Pycharm files

--- swap.py
+++ swap.py
@@ -1,12 +1,13 @@
 def three_variabe():
-    temp=0
+    temp = 0
     a = int(input("enter an integer a:"))
     b = int(input("enter an integer b:"))
-    temp=a
-    a=b
-    b=temp
+    temp = a
+    a = b
+    b = temp
     print("a =", a)
     print("b =", b)
+
 def add_swap():
     a = int(input("enter an integer a:"))
     b = int(input("enter an integer b:"))
@@ -15,6 +16,7 @@ def add_swap():
     a = a - b
     print("integer a is:", a)
     print("integer b is:", b)
+
 def mul_swap():
     a = float(input("enter an integer a:"))
     b = float(input("enter an integer b:"))
